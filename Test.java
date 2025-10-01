b9c9d721a5cc10fea47740b67da045ba38ff87b2
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2025-10-01 14:17:30 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
-        System.out.println("User 2 menambahkan baris baru");
-        System.out.println("User 3 menambah baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2"); //lia
+        System.out.println("User 2 menambahkan baris baru"); //lia
+        System.out.println("User 3 menambah baris baru"); //nahzul
     }
 }
