074dd5b46c569576ae8492670addfa4b84aa47c4
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2025-10-01 13:57:13 +0700

    Test04

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
+        System.out.println("User 2 menambahkan baris baru");
+    }
+}
