32180dc0b347f3e1a22b5ab12466dae66318a08f
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2025-10-01 14:27:18 +0700

    lia edit

--- Test.java
+++ Test.java
@@ -1,7 +1,10 @@
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2"); //lia
-        System.out.println("User 2 menambahkan baris baru"); //lia
-        System.out.println("User 3 menambah baris baru"); //nahzul
+        // user 1 = ayu
+        // user 2 = lia
+        // user 3 = nahzul
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
+        System.out.println("User 2 menambahkan baris baru"); 
+        System.out.println("User 3 menambah baris baru"); 
     }
 }
